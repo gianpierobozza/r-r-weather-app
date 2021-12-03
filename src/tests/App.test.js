9f0ffd037d3500d9849c88d912ebf40f41e031db
gianpierobozza <gianpiero.bozza@gmail.com>
2021-12-03 10:27:04 +0100

    used a few mui components, organized tests

---
+++ src/tests/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import Header from '../NavBar.js';
+
+test('renders navbar element', () => {
+  render(<Header />);
+  const navBarElement = screen.getByText(/React Navbar Example/i);
+  expect(navBarElement).toBeInTheDocument();
+});
+
+test('renders "Press Me" button', () => {
+  render(<App />);
+  const buttonElement = screen.getByText(/Press Me/i);
+  expect(buttonElement).toBeInTheDocument();
+});
