1f43754ab81d5e88c295d231ca9fa3e4ea3a63fe
gianpierobozza <gianpiero.bozza@gmail.com>
2021-12-04 13:59:00 +0100

    another environment variable test, with secret store in aws

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import Header from './NavBar.js';
 function App() {
   var getApiKey = () => {
     if (process.env.NODE_ENV === "development") {
+      console.log("it's development - " + process.env.NODE_ENV);
       return process.env.REACT_APP_OPENWEATHER_TEST_API_KEY;
     } else if (process.env.NODE_ENV === "production") {
+      console.log("it's production - " + process.env.NODE_ENV);
       return process.env.OPENWEATHER_PROD_API_KEY;
+    } else {
+      console.log("what is it? - " + process.env.NODE_ENV);
     }
   }
   console.log(getApiKey());
