bf70055b9b9c4b8c60e66dfe7a5cce229b852cda
gianpierobozza <gianpiero.bozza@gmail.com>
2021-12-27 10:54:10 +0100

    replaced text with icons in results, round numbers

--- src/components/CurrentWeatherSearchResults.js
+++ src/components/CurrentWeatherSearchResults.js
@@ -17,7 +17,13 @@ import ClearIcon from '@material-ui/icons/Clear';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ReplayIcon from '@material-ui/icons/Replay';
 
-const Item = styled(Paper)(({ theme }) => ({
+const ItemPaper = styled(Paper)(({ theme }) => ({
+	fontSize: 16,
+	padding: theme.spacing(1),
+	textAlign: "center",
+}));
+
+const Item = styled(Box)(({ theme }) => ({
 	fontSize: 16,
 	padding: theme.spacing(1),
 	textAlign: "center",
@@ -31,94 +37,117 @@ function convertDegreesToCardinalDir(deg) {
 	return directions[degrees];
 }
 
-const useStyles = makeStyles({
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+const mainIcon = makeStyles({
 	root: {
-		backgroundColor: "white",
 		border: 0,
 		borderRadius: 3,
-		margin: 12,
+		margin: 8,
+		width: 48,
+		height: 48
+	},
+});
+
+const weatherIcon = makeStyles({
+	root: {
+		border: 0,
+		borderRadius: 3,
+		margin: 4,
+		width: 32,
+		height: 32
 	},
 });
 
 const CurrentWeatherSearchResults = (props) => {
-	const img = useStyles();
+	const mainImg = mainIcon();
+	const weatherImg = weatherIcon();
 	var data = props.data;
 
 	return (
 		<Box>
 			<Grid container spacing={1}>
 				<Grid item xs={12}>
-					<Item>
+					<ItemPaper>
 						<Typography variant="h3" component="div">{data?.name}</Typography>
-						<img className={img.root} src={`https://openweathermap.org/img/wn/${data?.weather[0]?.icon}.png`} alt={data?.weather[0]?.icon} />
-					</Item>
+						<img className={mainImg.root} src={process.env.PUBLIC_URL + data?.weather[0]?.icon + ".png"} alt={data?.weather[0]?.icon} />
+						<Grid container justifyContent="flex-end">
+							<Grid item>
+								<ButtonGroup variant="contained" aria-label="outlined primary button group">
+									<Button
+										variant="contained"
+										onClick={props.handleCurrentWeatherRefreshClick}
+									>
+										<ReplayIcon />
+									</Button>
+									<Button
+										variant="contained"
+										onClick={props.handleClearWeatherClick}
+									>
+										<ClearIcon />
+									</Button>
+								</ButtonGroup>
+							</Grid>
+						</Grid>
+					</ItemPaper>
 				</Grid>
 				<Grid item xs={12}>
-					<Item>
+					<ItemPaper>
 						<FormattedMessage tagName="strong" id="current_weather_search_weather" values={{ value: data?.weather[0]?.description }} />
+					</ItemPaper>
+				</Grid>
+				<Grid item xs={3}>
+					<Item>
+						<img className={weatherImg.root} src={process.env.PUBLIC_URL + "thermometer.png"} alt="thermometer" />
+						<Typography component="div">{round(data?.main?.temp, 1)}&deg;C</Typography>
 					</Item>
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={3}>
 					<Item>
-						<FormattedMessage id="current_weather_search_temp" values={{ value: data?.main?.temp }} />&deg;C
+						<img className={weatherImg.root} src={process.env.PUBLIC_URL + "thermometer_feels_like.png"} alt="thermometer" />
+						<Typography component="div">{round(data?.main?.feels_like, 1)}&deg;C</Typography>
 					</Item>
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={3}>
 					<Item>
-						<FormattedMessage id="current_weather_search_temp_feels_like" values={{ value: data?.main?.feels_like }} />&deg;C
+						<img className={weatherImg.root} src={process.env.PUBLIC_URL + "thermometer_min.png"} alt="thermometer_min" />
+						<Typography component="div">{round(data?.main?.temp_min, 1)}&deg;C</Typography>
 					</Item>
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={3}>
 					<Item>
-						<FormattedMessage id="current_weather_search_temp_min" values={{ value: data?.main?.temp_min }} />&deg;C&nbsp;
+						<img className={weatherImg.root} src={process.env.PUBLIC_URL + "thermometer_max.png"} alt="thermometer_max" />
+						<Typography component="div">{round(data?.main?.temp_max, 1)}&deg;C</Typography>
 					</Item>
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={3}>
 					<Item>
-						<FormattedMessage id="current_weather_search_temp_max" values={{ value: data?.main?.temp_max }} />&deg;C
+						<img className={weatherImg.root} src={process.env.PUBLIC_URL + "pressure.png"} alt="pressure" />
+						<Typography component="div">{round(data?.main?.pressure)}mb</Typography>
 					</Item>
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={3}>
 					<Item>
-						<FormattedMessage id="current_weather_search_pressure" values={{ value: data?.main?.pressure }} />mb&nbsp;
+						<img className={weatherImg.root} src={process.env.PUBLIC_URL + "humidity.png"} alt="humidity" />
+						<Typography component="div">{round(data?.main?.humidity)}%</Typography>
 					</Item>
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={3}>
 					<Item>
-						<FormattedMessage id="current_weather_search_humidity" values={{ value: data?.main?.humidity }} />%
+						<img className={weatherImg.root} src={process.env.PUBLIC_URL + "wind_speed.png"} alt="wind_speed" />
+						<Typography component="div">{round(data?.wind?.speed, 1)}km/h</Typography>
 					</Item>
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={3}>
 					<Item>
-						<FormattedMessage id="current_weather_search_wind_speed" values={{ value: data?.wind?.speed }} />km/h&nbsp;
-					</Item>
-				</Grid>
-				<Grid item xs={6}>
-					<Item>
-						<FormattedMessage id="current_weather_search_wind_degrees" values={{ value: convertDegreesToCardinalDir(data?.wind?.deg) }} />
+						<img className={weatherImg.root} src={process.env.PUBLIC_URL + "wind_degrees.png"} alt="wind_degrees" />
+						<Typography component="div">{convertDegreesToCardinalDir(data?.wind?.deg)}</Typography>
 					</Item>
 				</Grid>
 			</Grid>
-			<Box sx={{ marginTop: 16, marginBottom: 16 }}>
-				<Grid container justifyContent="flex-end">
-					<Grid item md={4}>
-						<ButtonGroup variant="contained" aria-label="outlined primary button group">
-							<Button
-								variant="contained"
-								onClick={props.handleCurrentWeatherRefreshClick}
-							>
-								<ReplayIcon />
-							</Button>
-							<Button
-								variant="contained"
-								onClick={props.handleClearWeatherClick}
-							>
-								<ClearIcon />
-							</Button>
-						</ButtonGroup>
-					</Grid>
-				</Grid>
-			</Box>
 			<Accordion>
 				<AccordionSummary
 					expandIcon={<ExpandMoreIcon />}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 const Footer = () => {
 	return (
 		<div className="footer">
-			&copy; Gianpiero Bozza
+			<div>&copy; Gianpiero Bozza</div>
+			<div className="icons-attribution">
+				Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+			</div>
 		</div>
 	);
 };
