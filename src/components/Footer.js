f713fb6bb2b41bbf9270e6f42e79b99a8c6c9f2b
gianpierobozza <gianpiero.bozza@gmail.com>
2021-12-30 14:33:54 +0100

    add repo address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "@material-ui/core";
 const Footer = () => {
 	return (
 		<div className="footer">
-			<div>&copy; Gianpiero Bozza</div>
+			<div>&copy; Gianpiero Bozza - GitHub repository: <Link href="https://github.com/gianpierobozza/r-r-weather-app" color="inherit" target="_blank" rel="noopener">github.com/gianpierobozza/r-r-weather-app</Link></div>
 			<div className="icons-attribution">
 				Weather icons made by <Link href="https://www.flaticon.com/authors/pixel-perfect" color="inherit" target="_blank" rel="noopener">Pixel perfect</Link> from <Link href="https://www.flaticon.com/" color="inherit" target="_blank" rel="noopener">www.flaticon.com</Link>&nbsp;-
 				Sunset icon made by <Link href="https://www.flaticon.com/authors/freepik" color="inherit" target="_blank" rel="noopener">Freepik</Link> from <Link href="https://www.flaticon.com/" color="inherit" target="_blank" rel="noopener">www.flaticon.com</Link>
